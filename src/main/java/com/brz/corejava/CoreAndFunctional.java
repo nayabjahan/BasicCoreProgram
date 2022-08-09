343dd71dd1a26be3ac138a8b744cc711281f7661
nayab jahan <nayabzafir@gmail.com>
2022-08-09 13:16:19 +0530

    Added:Powers of two

--- src/main/java/com/brz/corejava/CoreAndFunctional.java
+++ src/main/java/com/brz/corejava/CoreAndFunctional.java
@@ -34,5 +34,31 @@ public class CoreAndFunctional {
             System.out.println("Entered Number is Odd");
         }
     }
-}
+    public static void powersOfTwo(String[] args){
+        int n = Integer.parseInt(args[0]);
+
+        int i = 0; // count from 0 to N
+
+        int powerOfTwo = 1; // the ith power of two
+
+// repeat until i equals n
+
+        while (i <= n) {
+
+            System.out.println(i + " " + powerOfTwo); // print out the power of two
+
+            powerOfTwo = 2 * powerOfTwo; // double to get the next one
+
+            i = i + 1;
+
+        }
+
+
+
+    }
+
+    }
+
+
+
 
--- src/main/java/com/brz/corejava/Main.java
+++ src/main/java/com/brz/corejava/Main.java
@@ -1,23 +1,27 @@
 package com.brz.corejava;
 
 import java.util.Scanner;
-import static com.brz.corejava.CoreAndFunctional.coinFlip;
-import static com.brz.corejava.CoreAndFunctional.evenOrOdd;
+
+import static com.brz.corejava.CoreAndFunctional.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("welcome to basic core java programming");
         System.out.println("Please Enter Your Choice ");
-        System.out.println("1.CoinFlip Programme \n 2.Even Or Odd Programme");
+        System.out.println("1.CoinFlip Programme \n 2.Even Or Odd Programme \n 3.powers of two");
 
         Scanner scanner = new Scanner(System.in);
         int option =scanner.nextInt();
+
         switch (option) {
             case 1:
                 coinFlip();
                 break;
             case 2:
                 evenOrOdd();
+                break;
+            case 3:
+                powersOfTwo(args);
                 break;
         }
     }
