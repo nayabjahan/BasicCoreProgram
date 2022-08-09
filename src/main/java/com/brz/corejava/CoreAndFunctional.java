b699f0e271eb1b6d67310abc7e637dbd53bb5cf4
nayab jahan <nayabzafir@gmail.com>
2022-08-09 20:53:05 +0530

    Added:largest number

--- src/main/java/com/brz/corejava/CoreAndFunctional.java
+++ src/main/java/com/brz/corejava/CoreAndFunctional.java
@@ -56,6 +56,31 @@ public class CoreAndFunctional {
         } else
             System.out.println("Specified year is not a leap year");
     }
+    public static void largestNumber(){
+        double X, Y, Z;
+
+        Scanner Check = new Scanner(System.in);
+        System.out.println("Enter the value for X: ");
+        X = Check.nextDouble();
+
+        System.out.println("Enter the value for Y: ");
+        Y = Check.nextDouble();
+
+        System.out.println("Enter the value for Z: ");
+        Z = Check.nextDouble();
+
+        if (X >= Y) {
+            if (X >= Z)
+                System.out.println("X = " + X + " is the largest number.");
+            else
+                System.out.println("Z = " + Z + " is the largest number.");
+        } else {
+            if (Y >= Z)
+                System.out.println("Y = " + Y + " is the largest number.");
+            else
+                System.out.println("Z = " + Z + " is the largest number.");
+        }
+    }
 }
 
 
--- src/main/java/com/brz/corejava/Main.java
+++ src/main/java/com/brz/corejava/Main.java
@@ -8,7 +8,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("welcome to basic core java programming");
         System.out.println("Please Enter Your Choice ");
-        System.out.println("1.CoinFlip Programme \n 2.Even Or Odd Programme \n 3.powers of two \n 4.Leap year");
+        System.out.println("1.CoinFlip Programme \n 2.Even Or Odd Programme \n 3.powers of two \n 4.Leap year" +
+                "\n 5. largest number");
 
         Scanner scanner = new Scanner(System.in);
         int option =scanner.nextInt();
@@ -25,6 +26,9 @@ public class Main {
                 break;
             case 4:
                 leapYear();
+                break;
+            case  5:
+                largestNumber();
                 break;
         }
     }
